73d0aed570d6192fd8aec7a8f2f6883edc886855
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-03-08 01:30:56 +0530

    Added solution - LeetHub

---
+++ Equilibrium index of an array - GFG/equilibrium-index-of-an-array.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+import java.util.*;
+class Index{
+public static void main(String[] args)
+ {
+	 Scanner sc = new Scanner(System.in);
+	 int T = sc.nextInt();
+		while(T-- > 0)
+		{
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for(int i=0;i<n;i++)
+			a[i] = sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.findEquilibrium(a,n));
+		}
+}
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+class GfG
+{
+           public static int findEquilibrium(int arr[], int n)
+              {
+                //add code here.
+               if(n==1){
+                   return 0;
+               }
+               
+               int sum=0;
+               int []s=new int[n];
+               for(int i=0;i<n;i++){
+                   sum+=arr[i];
+                   s[i]=sum;
+               }
+               
+               
+               for(int i=1;i<n;i++){
+                   if(s[i-1]==sum-s[i]){
+                       return i;
+                   }
+               }
+               return -1;
+              }
+}
