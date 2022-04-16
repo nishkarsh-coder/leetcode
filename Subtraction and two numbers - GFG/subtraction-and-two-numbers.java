7fd41b77b89f81e577e8b36e09ae32ca8f1fcd3c
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-04-16 20:10:51 +0530

    Added solution - LeetHub

---
+++ Subtraction and two numbers - GFG/subtraction-and-two-numbers.java
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[]=read.readLine().split(" ");
+            int A=Integer.parseInt(S[0]);
+            int B=Integer.parseInt(S[1]);
+            Solution ob = new Solution();
+            System.out.println(ob.repeatedSubtraction(A,B));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int repeatedSubtraction(int A,int B){
+        //code here
+        int step=0;
+        while(A>0&&B>0){
+            if(A<B){
+                B=B-A;
+                step++;
+            }
+            else{
+                A=A-B;
+                step++;
+            }
+        }
+        return step;
+    }
+}
