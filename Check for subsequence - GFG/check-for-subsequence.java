730e465d0f1d4fd4e40c6dfbe978022e12164db6
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-04-21 01:45:14 +0530

    Added solution - LeetHub

---
+++ Check for subsequence - GFG/check-for-subsequence.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            
+            String a = sc.next();
+            String b = sc.next();
+            Solution ob = new Solution();
+            if(ob.isSubSequence(a,b))
+            System.out.println(1);
+            else
+            System.out.println(0);
+ 		
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    boolean isSubSequence(String A, String B){
+      int i=0;
+      int j=0;
+      
+      while(j<B.length()&&i<A.length()){
+          if(B.charAt(j)==A.charAt(i)){
+              i++;
+          }
+          j++;
+      }
+      
+      return (i==A.length());
+      
+      
+    }
+}
