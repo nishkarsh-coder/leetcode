1818d88d013453736d60de962eab1dae581e74e8
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-05-31 13:58:47 +0530

    Time: 1 ms (99.61%), Space: 42 MB (18.26%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+                   long r=0;
+
+     
+
+           // int div=10;
+           // String ans="";
+           int ans=0;
+           while(x!=0){
+               r=r*10+ x%10;
+               x=x/10;
+           }
+       if(r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE)
+			return (int)r;
+		else
+			return 0;
+    }
+}
