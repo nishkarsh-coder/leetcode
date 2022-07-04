f5ff206845e3a0009e8f1fde1ec04a4b2769bd85
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-07-04 22:58:11 +0530

    Time: 26 ms (18.67%), Space: 48.2 MB (27.62%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,59 +1,34 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        HashMap<String,Integer> hm=new HashMap<>();
         for(int i=0;i<9;i++){
-        HashMap<Character,Integer> hm=new HashMap<>();
-            for(int j=0;j<9;j++){
-              // board
-                char c= board[i][j];
-                if(c=='.'){
-                    continue;
+            for(int j=0;j<9;j++)
+            {   char c= board[i][j];
+             if(c=='.'){
+                 continue;
+             }
+                String row="row"+c+i;
+                int n=(j/3)+(i/3)*3;
+                String box="box"+c+n;
+                String col="col"+c+j;
+             
+                if(hm.containsKey(row)){
+                    return false;
+                }else{
+                    hm.put(row,0);
                 }
-                    if(hm.containsKey(c)){
-                        return false;
-                    }
-                hm.put(c,0);
+             if(hm.containsKey(col)){
+                    return false;
+                }else{
+                    hm.put(col,0);
+                }
+             if(hm.containsKey(box)){
+                    return false;
+                }else{
+                    hm.put(box,0);
+                }
             }
         }
-        
-        
-        for(int i=0;i<9;i++){
-        HashMap<Character,Integer> hm=new HashMap<>();
-            for(int j=0;j<9;j++){
-              // board
-                char c= board[j][i];
-                if(c=='.'){
-                    continue;
-                }
-                    if(hm.containsKey(c)){
-                        return false;
-                    }
-                hm.put(c,0);
-            }
-        }
-        int col=0;
-        int row=-3;
-        for(int k=0;k<9;k++){        
-        HashMap<Character,Integer> hm=new HashMap<>();
-        if(k%3==0){
-           col=0;   
-            row+=3;
-        } 
-        for(int i=row;i<3+row;i++){
-         for(int j=col;j<3+col;j++){
-             char c= board[j][i];
-                if(c=='.'){
-                    continue;
-                }
-                    if(hm.containsKey(c)){
-                        return false;
-                    }
-                hm.put(c,0);
-        } 
-        }
-            col+=3;
-            // row+=3;
-                    }
-
         return true;
     }
 }
