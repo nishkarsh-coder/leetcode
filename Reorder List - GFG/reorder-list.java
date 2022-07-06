db76205789f0613b5e4a0d76d3066a4b2b4261d4
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-07-06 23:57:18 +0530

    Added solution - LeetHub

---
+++ Reorder List - GFG/reorder-list.java
@@ -0,0 +1,114 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+  public class ReorderList {
+    Node head; // head of lisl
+    Node last; // last of linked list
+
+    /* Linked list Node*/
+
+    /* Utility functions */
+
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            last = head;
+        } else {
+            // Node temp = head;
+            // while (temp.next != null) temp = temp.next;
+
+            last.next = node;
+            last = last.next;
+        }
+    }
+    /* Function to print linked list */
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            ReorderList llist = new ReorderList();
+
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            llist.head = new gfg().reorderlist(llist.head);
+
+            llist.printList();
+
+            t--;
+        }
+    }
+}// } Driver Code Ends
+
+
+/* Following is the Linked list node structure */
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class gfg {
+    Node reorderlist(Node head) {
+        // Your code here
+        Node temp=head;
+        int size=0;
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        left=head;
+        reorder(head,size,0);
+        return head;
+    }
+    Node left;
+      void reorder(Node head,int size,int floor) {
+       if(head==null){
+           return;
+       }
+       
+       reorder(head.next,size,floor+1);
+       if(floor>size/2){
+           head.next=left.next;
+           left.next=head;
+           left=left.next.next;
+       }else if(floor==size/2){
+           head.next=null;
+       }
+      }
+}
