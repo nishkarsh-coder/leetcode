e9a4606ec83915f7354213ee48765e0d9d83495a
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-07-11 03:40:06 +0530

    Added solution - LeetHub

---
+++ Remove duplicates from an unsorted linked list - GFG/remove-duplicates-from-an-unsorted-linked-list.java
@@ -0,0 +1,133 @@
+// { Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+    Node temp;
+	public void addToTheLast(Node node) 
+	{
+
+	  if (head == null) 
+	  {
+	    head = node;
+	    temp = node;
+	  }
+	  else{
+	      temp.next = node;
+	      temp = node;
+	  }
+	}
+
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+       
+        
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		  
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL();
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+		//llist.printList();	
+        Solution g = new Solution();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) 
+    {
+         // Your code here
+         if(head==null){
+             return head;
+         }
+        Node prev=head;
+         Node curr=head.next;
+    HashSet<Integer> h=new HashSet<>();
+         h.add(head.data);
+         while(curr!=null){
+        if(h.contains(curr.data)){
+
+        }else{
+            h.add(curr.data);
+            prev.next=curr;
+            prev=curr;
+        }
+        curr=curr.next;
+        
+         }
+        //  left=head;
+        prev.next=null;
+        return head;
+    }
+    // Node left;
+    // Node helper(Node node){
+    //     if(node==null){
+    //         return node;
+    //     }
+    //     boolean found=false;
+        
+    //     Node ans=helper(node.next);
+    //     if(found==true){
+    //         return ans;
+    //     }
+        
+    //     node.next=ans;
+    //     return node;
+    // }
+    
+}
