8916237c52df834c33055551f7589bd3729e6ac1
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-08-08 16:38:46 +0530

    Time: 26 ms (18.67%), Space: 42 MB (95.22%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -4,20 +4,25 @@ class Solution {
             return false;
         }
         HashMap<Character,Character> hm1=new HashMap<>();
-        HashMap<Character,Character> hm2=new HashMap<>();
+        HashMap<Character,Boolean> hm2=new HashMap<>();
         for(int i=0;i<s.length();i++){
             if(hm1.containsKey(s.charAt(i))&&hm1.get(s.charAt(i))!=t.charAt(i)){
                // System.out.println(i);
                 return false;
-            }else{
-            hm1.put(s.charAt(i),t.charAt(i));                
-            }
-            if(hm2.containsKey(t.charAt(i))&&hm2.get(t.charAt(i))!=s.charAt(i)){
-               // System.out.println(i);
+            }else if((hm1.containsKey(s.charAt(i))==false)&&hm2.containsKey(t.charAt(i))==true){
+                
+                // System.out.println("f"+" "+i+""+hm1.containsKey(s.charAt(i))+hm1);
                 return false;
             }else{
-            hm2.put(t.charAt(i),s.charAt(i));                
+            hm1.put(s.charAt(i),t.charAt(i));   
+                hm2.put(t.charAt(i),true);
             }
+            // if(hm2.containsKey(t.charAt(i))&&hm2.get(t.charAt(i))!=s.charAt(i)){
+            //    // System.out.println(i);
+            //     return false;
+            // }else{
+            // hm2.put(t.charAt(i),s.charAt(i));                
+            // }
         }
         return true;
     }
