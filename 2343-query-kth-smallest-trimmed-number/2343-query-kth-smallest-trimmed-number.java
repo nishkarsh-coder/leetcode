0565fb0918b728381a6fa8ca1fae633383fcc8ad
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-08-17 23:36:42 +0530

    Time: 449 ms (61.63%), Space: 117.7 MB (72.49%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.java
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.java
@@ -20,11 +20,9 @@ class Solution {
         }
         
         public int compareTo(pair other){
-             if (this.num.compareTo(other.num) == 0) {
-      return this.pos-other.pos;
-    } else {
+     
       return this.num.compareTo(other.num);
-    }
+ 
         }
         
      
@@ -34,16 +32,19 @@ class Solution {
     int countsort(String[] nums,int k,int trim){
         
         // HashMap<Integer,List<pair>> hm=new HashMap<>();
-        List<pair> ans=new ArrayList<>();
+        // List<pair> ans=new ArrayList<>();
+        pair[] ans=new pair[nums.length];
         for(int i=0;i<nums.length;i++){
             String str=nums[i];
             int l=str.length();
-            ans.add(new pair(str.substring(str.length()-trim),i));
+            // ans.add(new pair(str.substring(str.length()-trim),i));
+            ans[i]=new pair(str.substring(str.length()-trim),i);
         }
         
-       Collections.sort(ans);
+       // Collections.sort(ans);
+        Arrays.sort(ans);
+        return ans[k-1].pos;
         
-        
-        return ans.get(k-1).pos;
+        // return ans.get(k-1).pos;
     }
 }
