17c6e997eb936d59d9d07a40e53c5f312d0d9b0a
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-09-08 15:17:38 +0530

    Time: 21 ms (5.54%), Space: 55.3 MB (34.51%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,9 +1,53 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         ans=new ArrayList();
-        helper(graph,0,new ArrayList<>());
+        // helper(graph,0,new ArrayList<>());
+        bfs(graph);
         return ans;
     }
+    // class pair{
+    //     int n;
+    //     ArrayList<Integer> arr;
+    //     pair(int n,ArrayList<Integer> a){
+    //         n=this.n;
+    //         this.arr=a;
+    //     }
+    //     // pair(){
+    //     //   this.arr=new ArrayList<>();  
+    //     // }
+    // }
+    void bfs(int[][] graph){
+        Queue<ArrayList<Integer>> q=new ArrayDeque<>();
+                // Queue<Integer> q=new ArrayDeque<>();
+
+        ArrayList<Integer> a=new ArrayList<>();
+            a.add(0);
+        q.add(a);
+        // q.add(0);
+        
+        while(q.size()>0){
+            ArrayList<Integer> top=q.remove();
+           
+            if(top.get(top.size()-1)==graph.length-1){
+                ans.add(new ArrayList<>(top));
+            }
+            // System.out.println(q.size());
+        
+            for(int i :graph[top.get(top.size()-1)])
+            {   //System.out.println(i);
+                ArrayList<Integer>a1=new ArrayList<>(top);
+                    a1.add(i);
+                q.add(a1);
+            // System.out.println("///"+q.peek().n+".."+q.peek().arr+" "+q.size());
+
+            }      
+           
+                
+            // System.out.println(q.peek().n+".."+q.peek().arr+" "+q.size());
+
+        }
+    }
+    
     List<List<Integer>> ans;
     void helper(int[][] graph,int i,List<Integer> a){
         // System.out.println(i+": "+a+" ");
