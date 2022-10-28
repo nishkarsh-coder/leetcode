c04c13a8a455feaf51473802d5370cf0a420cf9f
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-10-29 00:34:59 +0530

    Time: 17 ms (25.78%), Space: 47.4 MB (56.59%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,18 +1,45 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int[][] dp=new int[matrix.length][matrix[0].length];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<matrix[0].length;i++){
-        ans=Math.min(ans,helper(i,0,matrix,dp)); 
-        //      for(int[] row:dp)
-        // System.out.println(Arrays.toString(row));
-        //     System.out.println();
-        }
+        // int[][] dp=new int[matrix.length][matrix[0].length];
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // int ans=Integer.MAX_VALUE;
+        // for(int i=0;i<matrix[0].length;i++){
+        // ans=Math.min(ans,helper(i,0,matrix,dp)); 
+        // //      for(int[] row:dp)
+        // // System.out.println(Arrays.toString(row));
+        // //     System.out.println();
+        // }
         // for(int[] row:dp)
         // System.out.println(Arrays.toString(row));
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] prev=new int[n];
+        for(int i=0;i<n;i++){
+            prev[i]=matrix[0][i];
+        }
+        // System.out.println(Arrays.toString(prev));
+        for(int i=1;i<m;i++){
+            int[] curr=new int[n];
+            for(int j=n-1;j>=0;j--){
+                if(j==n-1){
+                    // System.out.println(matrix[i][j]+" "+Math.min(prev[j],prev[j-1]));
+                    curr[j]=matrix[i][j]+Math.min(prev[j],prev[j-1]);
+                }else if(j==0){
+                    curr[j]=matrix[i][j]+Math.min(prev[j],prev[j+1]);
+                }else{
+                    curr[j]=matrix[i][j]+Math.min(prev[j],Math.min(prev[j+1],prev[j-1]));
+                }
+                 // System.out.println(Arrays.toString(curr));   
+            }
+            prev=curr;
+            // System.out.println(Arrays.toString(prev));
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            ans=Math.min(prev[i],ans);
+        }
         return ans;
     }
     
