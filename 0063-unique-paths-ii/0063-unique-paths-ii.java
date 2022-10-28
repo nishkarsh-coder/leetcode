457d81975ee5b59dac4fb53bcba9359e94f09ff0
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-10-28 21:40:55 +0530

    Time: 1 ms (78.40%), Space: 41.6 MB (80.06%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,42 +2,25 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n=obstacleGrid.length;
         int m=obstacleGrid[0].length;
-        int[][] dp=new int[n][m];
-        // System.out.println(n+" "+m+" "+Arrays.toString(z));
-        dp[n-1][m-1]=1;
-        
-        if(obstacleGrid[n-1][m-1]==1){
-            return 0;
+        int[] prev=new int[m+1];
+        for(int i=m-1;i>=0;i--){
+            if(i==m-1&&obstacleGrid[n-1][i]!=1){
+            prev[i]=1;                
+            }else if(obstacleGrid[n-1][i]!=1){
+                prev[i]=prev[i+1];
+            }
         }
         
-        for(int i=n-1;i>=0;i--){
-        for(int j=m-1;j>=0;j--){
-           if(i==n-1&&j==m-1){
-               
-           }else if(i!=n-1&&j!=m-1){
-               if(obstacleGrid[i][j]==1){
-                   dp[i][j]=0;
-               }else{
-                   dp[i][j]=dp[i+1][j]+dp[i][j+1];
-               }
-           }else if(i==n-1){
-               if(obstacleGrid[i][j]==1||dp[i][j+1]==-1){
-                   dp[i][j]=0;
-               }else{
-                    dp[i][j]=dp[i][j+1];
-               }
-           }else if(j==m-1){
-               if(obstacleGrid[i][j]==1||dp[i+1][j]==-1){
-                   dp[i][j]=0;
-               }else{
-                    dp[i][j]=dp[i+1][j];
-               }
-           }
-        }    
+        for(int i=n-2;i>=0;i--){
+            int[] curr=new int[m+1];
+            for(int j=m-1;j>=0;j--){
+                if(obstacleGrid[i][j]!=1){
+                    curr[j]=prev[j]+curr[j+1];
+                }
+            }
+            prev=curr;
         }
         
-        if(dp[0][0]==-1) return 0;
-        return dp[0][0];
-        
+        return prev[0];
     }
 }
