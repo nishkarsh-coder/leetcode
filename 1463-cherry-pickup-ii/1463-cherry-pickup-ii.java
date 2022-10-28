f8cf575e9a0791ec7003b2b67fb007dbb0f6a2a8
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-10-29 02:16:55 +0530

    Time: 68 ms (29.25%), Space: 48.7 MB (44.73%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -2,13 +2,46 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int[][][] dp=new int[m][m][n];
+        int[][][] dp=new int[n][m][m];
         for(int[][] row: dp){
             for(int[] r:row){
         Arrays.fill(r,-1);                            
             }
         }
-        return helper(0,m-1,0,grid,dp);
+        // return helper(0,m-1,0,grid,dp);
+        for(int i=0;i<m;i++){  //col1
+            for(int j=0;j<m;j++){  //col2
+                if(i==j){
+                dp[n-1][i][j]=grid[n-1][i] ;                   
+                }else{
+            dp[n-1][i][j]=grid[n-1][i]+grid[n-1][j];                   
+                }
+            }
+        }
+        
+        for(int k=n-2;k>=0;k--){
+         for(int i=0;i<m;i++){  //col1
+            for(int j=0;j<m;j++){  //col2
+                int ans=0;
+                for(int row=-1;row<2;row++){
+                    for(int col=-1;col<2;col++){
+                        if(i+row>=0&&j+col>=0&&i+row<m&&j+col<m){
+                        ans=Math.max(ans,dp[k+1][i+row][j+col]);
+                        }
+                    }
+                }
+                
+                if(i==j){
+                dp[k][i][j]=grid[k][i]+ans;                   
+                }else{
+                dp[k][i][j]=grid[k][i]+grid[k][j]+ans;                   
+                }
+            }
+        }   
+        }
+        
+        return dp[0][0][m-1];
+        
     }
    int helper(int col1,int col2,int row,int[][] grid,int[][][] dp){
         if(col1<0||col2<0||col1>=grid[0].length||col2>=grid[0].length||row>=grid.length){
