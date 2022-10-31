4ab437db2023d788426d228ddec5ae2f85f16fb7
nishkarsh-coder <68010352+nishkarsh-coder@users.noreply.github.com>
2022-10-31 15:35:09 +0530

    Added solution - LeetHub

---
+++ Minimum number of Coins - GFG/minimum-number-of-coins.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            List<Integer> numbers= new ArrayList<Integer>();
+            numbers = ob.minPartition(N);
+            for(int i: numbers){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static List<Integer> minPartition(int N)
+    {
+        // code here
+        int[] coin={2000,500,200,100,50,20,10,5,2,1};
+        // coin[0]=1;
+        int freq=0;
+        List<Integer> ans=new ArrayList<>();
+        int i=0;
+        // for(int i=0;i<10;i++){
+            while(N>0&&i>=0){
+                if(coin[i]<=N){
+                    // System.out.println(N+" "+freq+" "+coin[i]+"ssss");
+                    freq+=N/coin[i];
+                    N=N%coin[i];
+                    // System.out.println(N+" "+freq+" "+coin[i]);
+                    while(freq>0){
+                        ans.add(coin[i]);
+                        // System.out.println(N+" "+freq+" "+coin[i]+".");
+                        freq--;
+                    }
+                    // System.out.println(N+" "+freq+" "+coin[i]+">>.");
+                }
+                i++;
+            }
+        
+        return ans;
+    }
+}
